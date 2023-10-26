ab490fab5626fb4851d132c1e97ff0bb939eeac6
chigoox <dikeemmanuel54@gmail.com>
2023-10-26 14:00:20 -0400

    up

--- app/Componets/HomePage/HomeProtuctCategories.jsx
+++ app/Componets/HomePage/HomeProtuctCategories.jsx
@@ -26,7 +26,7 @@ const HomeProtuctCategories = () => {
 
             className="grid grid-cols-2 md:grid-cols-4 items-center justify-center   w-full h-80 relative">
             {category.map((category) => (
-                <Link key={category} href={`/Shop/${category.includes('Hot') ? 'HotTools' : category.replace(/\s/g, '')}`} className=" hover:border-white hover:font-extrabold hover:border-2 trans-fast  drop-shadow-md shadow-black m-auto overflow-hidden rounded-full md:w-[50%] md:h-[100%]  w-[90%] h-[70%] relative">
+                <Link key={category} href={`/Shop/${category.includes('Hot') ? 'HotTools' : category.replace(/\s/g, '')}`} className=" hover:border-white hover:font-extrabold  trans-fast  drop-shadow-md shadow-black m-auto overflow-hidden rounded-full md:w-[50%] md:h-[100%] border-gray-900 border-2  w-[50%] h-[70%] relative">
                     <Image width='300' height='900' className='h-full w-full  object-cover'
                         src={HomePageCategoryImages(category)} alt="" />
                     <div className="absolute top-0 m-auto h-full center w-full">
